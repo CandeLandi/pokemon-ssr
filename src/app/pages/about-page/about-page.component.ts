cc936a2b5933df407f0deebd28fb22c9e7d2c7ce
Candela Landi <landicandela01@gmail.com>
2025-01-26 11:18:45 -0300

    ssr finished

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about-page',
   standalone: true,
   imports: [],
   templateUrl: './about-page.component.html',
-  styleUrl: './about-page.component.css'
+  styleUrl: './about-page.component.css',
 })
-export default class AboutPageComponent {
+export default class AboutPageComponent implements OnInit {
+  private title = inject(Title);
+  private meta = inject(Meta);
 
+  ngOnInit(): void {
+    this.title.setTitle('Sobre la aplicación');
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Sobre la aplicación',
+    });
+
+    this.meta.updateTag({ name: 'og:title', content: 'About Page' });
+    this.meta.updateTag({
+      name: 'keywords',
+      content: 'Hola,Mundo,Candela,Landi,Angular'
+    })
+  }
 }
--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact-page',
@@ -7,6 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.css'
 })
-export default class ContactPageComponent {
+export default class ContactPageComponent implements OnInit{
+
+  private title = inject(Title);
+  private meta = inject(Meta)
+
+
+  ngOnInit(): void {
+    this.title.setTitle('Contacto');
+    this.meta.updateTag({name: 'description', content: 'Contacto de la app'});
+
+    this.meta.updateTag({ name: 'og:title', content: 'About Page' });
+    this.meta.updateTag({
+      name: 'keywords',
+      content: 'Hola,Mundo,Candela,Landi,Angular'
+    })
+  }
 
 }
